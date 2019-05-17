39a8da1c130bb3e711e20f9b246097c393fd9932
Sergey Kaluzhskiy <sergey@ortoped.org.ru>
2019-05-17 08:46:52 +0400

    implemented file uploading

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import settings
 from telethon.client.telegramclient import TelegramClient
 import asyncio
+import sys
 
 
+help = "Usage: python main.py <mode> <chat_id> <filename>\nwhere mode is:\n--get-chats  -  get chat list for obtaining chat id\n--upload-file  -  upload file to specified chat"
+
+if len(sys.argv) == 1:
+	print(help)
+	sys.exit()
+	
 loop = asyncio.get_event_loop()
-
 
 async def main():
 	client = None
@@ -13,12 +19,27 @@ async def main():
 	else:
 		client = TelegramClient('telegramFileUploader', settings.api_id, settings.api_hash)
 	await client.start()
+
+	if sys.argv[1] == "--get-chats":
+		dialogs = await client.get_dialogs()
+		for dialog in dialogs:
+			print("%s - %s" % (dialog.name, dialog.entity.id))
 	
-	dialogs = await client.get_dialogs()
-	
-	for dialog in dialogs:
-		print("%s - %s" % (dialog.name, dialog.entity.id))
-	
+	elif sys.argv[1] == "--upload-file":
+		chat_id = sys.argv[2]
+		filename = sys.argv[3]
+		entity = None
+		dialogs = await client.get_dialogs()
+		for dialog in dialogs:
+			id = str(dialog.entity.id)
+			if id == chat_id:
+				entity = dialog.entity
+				break
+		if not entity:
+			print("chat with id %s not found" % chat_id)
+			sys.exit(1)
+		result = await client.send_file(entity, filename)
+# 		print(result)
 
 
 loop.run_until_complete(main())
--- readme.txt
+++ readme.txt
@@ -1,1 +1,5 @@
 Simple command-line tool for uploading files to telegram channels
+
+Usage: python main.py <mode> <chat_id> <filename>\nwhere mode is:
+--get-chats  -  get chat list for obtaining chat id
+--upload-file  -  upload file to specified chat
